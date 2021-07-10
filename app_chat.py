177fa69125569b370fca30ae7c479c5d828b06c5
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2021-07-10 22:43:10 +0900

    Install streamlit>=0.84.0 for app_chat.py to use st.sesson_state

--- app_chat.py
+++ app_chat.py
@@ -4,13 +4,14 @@ from streamlit_server_state import use_server_state
 
 
 def main():
+    def on_message_input():
+        new_message = st.session_state["message_input"]
+        chat_messages = chat_messages_state.get_value() + [new_message]
+        chat_messages_state.set_value(chat_messages)
+
     chat_messages_state = use_server_state("chat_messages", [])
 
-    message = st.text_input("Message")
-    send = st.button("Send")
-    if message and send:
-        chat_messages = chat_messages_state.get_value() + [message]
-        chat_messages_state.set_value(chat_messages)
+    st.text_input("Message", key="message_input", on_change=on_message_input)
 
     st.write(chat_messages_state.get_value())
 
