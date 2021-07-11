91ea2fc65860d7d55f626721d6ff6a168fe2ea1b
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2021-07-11 18:08:52 +0900

    Update sample chats (#6)
    * Update sample chats
    
    * Update samples to use locks
    
    * Improve app_chat_rooms.py

--- app_chat.py
+++ app_chat.py
@@ -1,13 +1,26 @@
 import streamlit as st
 
-from streamlit_server_state import server_state
+from streamlit_server_state import server_state, server_state_lock
 
 
 def main():
+    nickname = st.text_input("Nick name", key="nickname")
+    if not nickname:
+        return
+
     def on_message_input():
-        new_message = st.session_state["message_input"]
-        chat_messages = server_state["chat_messages"] + [new_message]
-        server_state["chat_messages"] = chat_messages
+        new_message_text = st.session_state["message_input"]
+        if not new_message_text:
+            return
+
+        new_message_packet = {
+            "nickname": nickname,
+            "text": new_message_text,
+        }
+        with server_state_lock["chat_messages"]:
+            server_state["chat_messages"] = server_state["chat_messages"] + [
+                new_message_packet
+            ]
 
     if "chat_messages" not in server_state:
         server_state["chat_messages"] = []
@@ -15,10 +28,6 @@ def main():
     st.text_input("Message", key="message_input", on_change=on_message_input)
 
     st.write(server_state["chat_messages"])
-
-    for k in server_state:
-        st.write(k)
-    st.write(st.session_state)
 
 
 if __name__ == "__main__":
---
+++ app_chat_rooms.py
@@ -0,0 +1,60 @@
+import streamlit as st
+
+from streamlit_server_state import server_state, server_state_lock
+
+
+def main():
+    if "rooms" not in server_state:
+        server_state["rooms"] = []
+
+    rooms = server_state["rooms"]
+
+    room = st.sidebar.radio("Select room", rooms)
+
+    with st.sidebar.form("New room"):
+
+        def on_create():
+            new_room_name = st.session_state.new_room_name
+            with server_state_lock["rooms"]:
+                server_state["rooms"] = server_state["rooms"] + [new_room_name]
+
+        st.text_input("Room name", key="new_room_name")
+        st.form_submit_button("Create a new room", on_click=on_create)
+
+    if not room:
+        return
+
+    room_key = f"room_{room}"
+    if room_key not in server_state:
+        server_state[room_key] = []
+
+    st.header(room)
+
+    nickname = st.text_input("Nick name", key=f"nickname_{room}")
+    if not nickname:
+        st.warning("Set your nick name.")
+        return
+
+    message_input_key = f"message_input_{room}"
+
+    def on_message_input():
+        new_message_text = st.session_state[message_input_key]
+        if not new_message_text:
+            return
+
+        new_message_packet = {
+            "nickname": nickname,
+            "text": new_message_text,
+        }
+
+        with server_state_lock[room_key]:
+            server_state[room_key] = server_state[room_key] + [new_message_packet]
+
+    st.text_input("Message", key=message_input_key, on_change=on_message_input)
+
+    st.subheader("Messages:")
+    st.write(server_state[room_key])
+
+
+if __name__ == "__main__":
+    main()
