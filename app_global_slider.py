017f3b394d18bcb88cf77d1ade05d30a85b7d9a2
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2021-09-09 20:04:11 +0900

    Fix the slider exmaple and update README.md

--- app_global_slider.py
+++ app_global_slider.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 from streamlit_server_state import server_state, server_state_lock
 
-st.title("Globally Shared Widget Example")
+st.title("Globally Shared Slider Example")
 st.markdown(
     "Open this app in multiple tabs/windows and "
     "see the slider value is shared and synchronized across the sessions."
