447f62556b6025c9bb87a25112e01f92fda49be1
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2021-09-10 19:29:12 +0900

    Update server_state_lock_test.py

--- tests/server_state_lock_test.py
+++ tests/server_state_lock_test.py
@@ -6,7 +6,14 @@ def test_lock_object_has_the_same_keys():
     server_state = ServerState()
     server_state_lock = ServerStateLock(server_state)
 
+    assert len(server_state_lock) == 0
+    assert "foo" not in server_state_lock
+    assert "bar" not in server_state_lock
+    assert list(server_state_lock) == []
+
     server_state["foo"] = 42
 
+    assert len(server_state_lock) == 1
     assert "foo" in server_state_lock
     assert "bar" not in server_state_lock
+    assert list(server_state_lock) == ["foo"]
